5a05e62e8416ea48a9a92eb8d2c146a6ac774a1d
joavega21 <isnp151323@ugb.edu.sv>
2024-04-27 02:38:05 -0700

    Actividad correspondiente a la semana 2 de materia Programacion II 2024

--- ISNP151323_Unidad2/ISNP151323_Unidad2/ejercicio8.Designer.cs
+++ ISNP151323_Unidad2/ISNP151323_Unidad2/ejercicio8.Designer.cs
@@ -24,12 +24,99 @@ namespace ISNP151323_Unidad2 {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.lblTabla = new System.Windows.Forms.Label();
+            this.txtTabla = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.lsbTabla = new System.Windows.Forms.ListBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.Location = new System.Drawing.Point(81, 28);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(378, 27);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.Text = "Tabla de multiplicar de un numero";
+            // 
+            // lblTabla
+            // 
+            this.lblTabla.AutoSize = true;
+            this.lblTabla.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTabla.Location = new System.Drawing.Point(36, 97);
+            this.lblTabla.Name = "lblTabla";
+            this.lblTabla.Size = new System.Drawing.Size(326, 22);
+            this.lblTabla.TabIndex = 1;
+            this.lblTabla.Text = "Ingrese numero para generar su tabla";
+            // 
+            // txtTabla
+            // 
+            this.txtTabla.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTabla.Location = new System.Drawing.Point(390, 96);
+            this.txtTabla.Name = "txtTabla";
+            this.txtTabla.Size = new System.Drawing.Size(160, 28);
+            this.txtTabla.TabIndex = 2;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(159, 147);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(101, 38);
+            this.btnGenerar.TabIndex = 3;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // lsbTabla
+            // 
+            this.lsbTabla.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbTabla.FormattingEnabled = true;
+            this.lsbTabla.ItemHeight = 21;
+            this.lsbTabla.Location = new System.Drawing.Point(159, 225);
+            this.lsbTabla.Name = "lsbTabla";
+            this.lsbTabla.Size = new System.Drawing.Size(229, 277);
+            this.lsbTabla.TabIndex = 4;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(287, 147);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(101, 38);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // ejercicio8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(574, 522);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lsbTabla);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtTabla);
+            this.Controls.Add(this.lblTabla);
+            this.Controls.Add(this.lblSaludo);
+            this.Name = "ejercicio8";
             this.Text = "ejercicio8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Label lblTabla;
+        private System.Windows.Forms.TextBox txtTabla;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.ListBox lsbTabla;
+        private System.Windows.Forms.Button btnLimpiar;
     }
 }
--- ISNP151323_Unidad2/ISNP151323_Unidad2/ejercicio8.cs
+++ ISNP151323_Unidad2/ISNP151323_Unidad2/ejercicio8.cs
@@ -13,5 +13,20 @@ namespace ISNP151323_Unidad2 {
         public ejercicio8() {
             InitializeComponent();
         }
+
+        private void btnLimpiar_Click(object sender, EventArgs e) {
+            txtTabla.Clear();
+            lsbTabla.Items.Clear();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e) {
+            int num = int.Parse(txtTabla.Text);
+            int producto = 0, i = 1;
+            do {
+                producto = num * i;
+                lsbTabla.Items.Add(num + " X " + i + " = " + producto);
+                i++;
+            } while (i <= 10);
+        }
     }
 }
